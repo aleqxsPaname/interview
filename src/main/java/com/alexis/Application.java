56785a93bdda763a37f2974104d605fcec478a90
alexisdebeauvais <alexisdebeauvais@gmail.com>
2020-04-08 13:17:05 +0200

    first commit

---
+++ src/main/java/com/alexis/Application.java
@@ -0,0 +1,24 @@
+package com.alexis;
+
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+
+    public static void main(String[] args) {
+        System.out.println("DISPLAY BEFORE SPRING CONTEXT LOADING");
+        SpringApplication.run(Application.class, args);
+    }
+
+    public void run(String... args) {
+        System.out.println("DISPLAY AFTER SPRING CONTEXT LOADING");
+
+    }
+
+
+}
---
+++ src/main/java/com/alexis/rte/rte.java
@@ -0,0 +1,7 @@
+package com.alexis.rte;
+
+public class rte {
+
+
+
+}
